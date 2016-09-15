02529655b742088d9c289977d2d7debf1e65201c
evandersar <evandersar@ukr.net>
2016-09-16 00:08:33 +0300

    class tasks for 24 lesson are done

---
+++ lesson_24/Classwork/jsx/main1.jsx
@@ -0,0 +1,42 @@
+﻿var React = require('react'); 
+var ReactDOM = require('react-dom'); 
+
+// импорт необходимых для настройки маршрутизации объектов из модуля react-router
+var router = require('react-router'); 
+
+var Router = router.Router; 
+var Route = router.Route; 
+var Link = router.Link; 
+var IndexRoute = router.IndexRoute; 
+var hashHistory = router.hashHistory; 
+
+// импорт компонентов, переключаемых при маршрутизации(views)
+var View1 = require('./views/view1.jsx'); 
+var View2 = require('./views/view2.jsx');  
+
+// В данном примере рассмотрена базовая настройка декларативной маршрутизации с помощью react-router 
+
+class App extends React.Component {
+    render() {
+        return (
+        <div>
+            <div id="container" className="panel well">
+                <h1>React Routing Demo!</h1>
+                     <ul>
+                          <li><Link to="/view1"><button className="btn btn-lg btn-success">View 1</button></Link></li>
+                          <li><Link to="/view2"><button className="btn btn-lg btn-success">View 2</button></Link></li>
+                    </ul>
+            </div>  
+            <div className="panel">{this.props.children}</div>
+        </div>
+   )}
+} 
+
+// конфигурация маршрутизации - набор инструкций, которые устанавливают связь между URL и кодом приложения. 
+ReactDOM.render(
+     <Router history={hashHistory}>
+      <Route path='/' component={App}> 
+        <Route path="view1" component={View1} />
+        <Route path="view2" component={View2} />
+    </Route>
+    </Router>, document.getElementById('app')); 
